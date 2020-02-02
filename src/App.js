a7bcc3ea800ace16a949a6dd161e0cdf036e2e47
LI Chuan <lichuan172@gmail.com>
2020-02-02 20:20:52 +0800

    finish a simple redux

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-function App() {
-  return <div id="app">App</div>;
-}
-
-render(<App />, document.getElementById("root"));
---
+++ src/redux/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+export function increment() {
+  return {
+    type: "INCREMENT"
+  };
+}
+
+export function decrement() {
+  return {
+    type: "DECREMENT"
+  };
+}
+
+function countReducer(count = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return count + 1;
+    case "DECREMENT":
+      return count - 1;
+    default:
+      return count;
+  }
+}
+
+const store = createStore(countReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+
+export default store;
